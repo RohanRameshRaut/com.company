2627de6e41807abcf9fbb57faf956270d6d947e0
RohanRameshRaut <rohanraut918@gmail.com>
2024-06-15 11:54:36 +0530

    Create CWH_51_exercise04.java

---
+++ CWH_51_exercise04.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class CWH_51_exercise04 {
+    public static void main(String[] args) {
+        // You have to implement a library using Java Class Library
+        // Methods: addBook, issueBook, returnBook, showAvailableBooks
+        // Properties: Array to store the available books,
+        // Array to store the issued books.
+
+    }
+}
