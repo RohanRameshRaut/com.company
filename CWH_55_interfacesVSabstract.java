98c1087be88e34e72dd783790396ba0f9d4f944c
RohanRameshRaut <rohanraut918@gmail.com>
2024-06-17 14:31:40 +0530

    Create CWH_55_interfacesVSabstract.java

---
+++ CWH_55_interfacesVSabstract.java
@@ -0,0 +1,18 @@
+package com.company;
+
+/*
+*  Why use interfaces when we have abstract?
+* --> As we know Java does not support multiple inheritance (many parents one child).
+* --> Abstract class follows the same above rule but interfaces does not follow the above rule
+* --> We can create a single implements class from multiple interfaces class
+* --> Abstract can have (only one parent many child)
+* --> Interfaces can have (many parents many child) like multiple inheritance
+* --> Abstract class can contain other methods also
+* --> Interfaces can only contain the methods which are important
+*
+* */
+public class CWH_55_interfacesVSabstract {
+    public static void main(String[] args) {
+
+    }
+}
